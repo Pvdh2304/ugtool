5493da14ae83ef3ccdf265c8d1741eab6e6ff13a
Phạm Văn Đức Hạnh <137122662+Pvdh2304@users.noreply.github.com>
2025-01-12 21:43:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i python-pip && pip install pip install shutil requests pytz
+termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i python-pip && pip install shutil requests pytz
 curl -Ls "https://raw.githubusercontent.com/Pvdh2304/ugtool/refs/heads/main/ugtool.py" -o /sdcard/Download/ugtool.py
