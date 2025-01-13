2f36331be5119b5fdbfd0cc9593d931a2444acab
Phạm Văn Đức Hạnh <137122662+Pvdh2304@users.noreply.github.com>
2025-01-13 08:05:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i python-pip && pip install requests pytz
-curl -Ls "https://raw.githubusercontent.com/Pvdh2304/ugtool/refs/heads/main/ugtool.py" -o /sdcard/Download/ugtool.py | python /sdcard/Download/ugtool.py
+curl -Ls "https://raw.githubusercontent.com/Pvdh2304/ugtool/refs/heads/main/ugtool.py" -o /sdcard/Download/ugtool.py && python /sdcard/Download/ugtool.py
